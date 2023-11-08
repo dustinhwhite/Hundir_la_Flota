9c4ac1b625a533ac4d7df369153144e66be08550
Dustin <124713620+dustinhwhite@users.noreply.github.com>
2023-11-08 14:26:41 +0100

    Cuadrado descripcion

---
+++ Battleships_OOP/src/cuadrado.py
@@ -0,0 +1,4 @@
+# Estado: un cuadrado puede tener un estado que indique si está vacío, tiene un barco, ha sido golpeado, ha sido un fallo etc.
+# Coordenadas: almacenar sus propias coordenadas (fila, columna).
+# Contenido: puede llevar un registro de lo que contiene (una parte de un barco o algo así).
+# Visualización: puede determinar como en el tablero debe ser mostrado (caracteres para vacío(agua), barco, golpe o fallo).
