1ae6a75fe9b4920cceeacdfb42b4c9d93303f1a0
BeatrizGD <beatriz.bgd@gmail.com>
2023-11-13 12:28:10 +0100

    Function initial_board created

--- Battleships_OOP/src/funciones.py
+++ Battleships_OOP/src/funciones.py
@@ -1,4 +1,11 @@
-# Para funciones no incluidos en las Clases
+# Funciones no incluidas en las Clases
+
+import numpy as np
+
+def initial_board():
+    init_board = np.full((10,10), "O")
+    return init_board
 
 
+# def display_boards(player_board, player_tracking_board):
 
