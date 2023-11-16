6f2f646a5627f90002a2f6114be68630c01ec159
Dustin <124713620+dustinhwhite@users.noreply.github.com>
2023-11-16 14:20:19 +0100

    Board Orientation

--- Battleships_OOP/src/classes.py
+++ Battleships_OOP/src/classes.py
@@ -17,42 +17,54 @@ class Board:
         self.boats_placed = {key: 0 for key in boat_sizes} # Contador de boats in boat sizes, cada valor empieza con cero
 
     
-    def is_space_free (self, row, col, boat_size, is_horizontal):
-        if is_horizontal: 
-            if col + boat_size > self.size:
+    def is_space_free (self, row, col, boat_size, orientation):
+        if orientation == 'E':  # East
+            if col + boat_size > self.size: # Check if end position of the boat is off the board
                 return False
-            return np.all(self.board[row, col: col + boat_size] == "~") # Check if the horizontal space is free
-        else:
+            return np.all(self.board[row, col: col + boat_size] == "~")
+        elif orientation == 'W':  # West
+            if col - boat_size < -1:    
+                return False
+            return np.all(self.board[row, col - boat_size + 1: col + 1] == "~")
+        elif orientation == 'S':  # South
             if row + boat_size > self.size:
                 return False
-            return np.all(self.board[row: row + boat_size, col] == "~") # Check if the vertical space is free
+            return np.all(self.board[row: row + boat_size, col] == "~")
+        else:  # North
+            if row - boat_size < -1:
+                return False
+            return np.all(self.board[row - boat_size + 1: row + 1, col] == "~") # Check if the vertical space is free
 
-
-    def place_boat(self, row, col, boat_size, is_horizontal): 
-        if is_horizontal: 
+    def place_boat(self, row, col, boat_size, orientation): 
+        if orientation == 'E':  # East
             for i in range(boat_size):
                 self.board[row, col + i] = self.boat_symbol
-        else: 
+        elif orientation == 'W':  # West
             for i in range(boat_size):
-                self.board[row + i, col] = self.boat_symbol 
-
+                self.board[row, col - i] = self.boat_symbol
+        elif orientation == 'S':  # South
+            for i in range(boat_size):
+                self.board[row + i, col] = self.boat_symbol
+        else:  # North
+            for i in range(boat_size):
+                self.board[row - i, col] = self.boat_symbol
 
     def place_all_boats(self):
         for boat_size in self.boat_sizes:   # Go through each boat size defined in BOAT_SIZES
             required_boats_count = self.boat_sizes[boat_size]   # Get the number of boats of this size we need to place
 
             while self.boats_placed[boat_size] < required_boats_count:  # Keep placing boats until we have placed the required #
-                is_horizontal = random.choice([True, False])    # Generates is_horizontal variable random choice
+                orientation = random.choice(['N', 'S', 'E', 'O'])    # Generates orientation variable random choice
                 row = np.random.randint(0, self.size)   # Generate a random starting position for the boat
                 col = np.random.randint(0, self.size)
 
-                if is_horizontal:   
+                if orientation:   
                     col = np.random.randint(0, self.size - boat_size + 1)
                 else:
                     row = np.random.randint(0, self.size - boat_size + 1)
             
-                if self.is_space_free (row, col, boat_size, is_horizontal): # Check if the boat can be placed at the starting position
-                    self.place_boat(row, col, boat_size, is_horizontal) # If it can, place the boat on the board
+                if self.is_space_free (row, col, boat_size, orientation): # Check if the boat can be placed at the starting position
+                    self.place_boat(row, col, boat_size, orientation) # If it can, place the boat on the board
                     self.boats_placed[boat_size] += 1   # Increase the count of boats placed for this size
 
 
