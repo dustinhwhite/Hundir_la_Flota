f88826b5ff6e921497c2cdf033241405e21ce420
BeatrizGD <beatriz.bgd@gmail.com>
2023-11-17 10:16:04 +0100

    Variables modificadas

--- Battleships_OOP/src/variables.py
+++ Battleships_OOP/src/variables.py
@@ -3,12 +3,17 @@
     such as board size, ship types and sizes, and any other constants.
 '''
 SIZE = 10
+
 BOAT1 = 4
 BOAT2 = 3
 BOAT3 = 2
 BOAT4 = 1
-WATER = "~"
-BOAT ="B"          
-MISS = "."
-BOAT_DAMAGED = "X"
+
+WATER = "💧"
+BOAT ="🚢"
+MISS = "💥"
+BOAT_DAMAGED =  "💀"
+
 BOAT_SIZES = {1: 4, 2: 3, 3: 2, 4: 1}  # Key = Boat size, Value = Number of boats
+
+row_column_names = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5, 'F': 6, 'G': 7, 'H': 8, 'I': 9, 'J': 10}
